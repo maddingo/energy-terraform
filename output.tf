12c1730ed716c42862ee918387c0d9ee1afa46fa
Martin Goldhahn <mgoldhahn@gmail.com>
2018-12-01 22:53:34 +0100

    removed instance

--- output.tf
+++ output.tf
@@ -1,7 +1,3 @@
-output "ip" {
-    value = "${google_compute_instance.vm_instance.network_interface.0.access_config.0.nat_ip}"
-}
-
 output "gcp_ssh_command" {
     value = "ssh ${var.cluster_admin_username}@${google_container_cluster.energy.endpoint}"
 }
